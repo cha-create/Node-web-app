83d076925edca954968f8d4d41f6fff87b3097e8
Charley griffin <boingr66@gmail.com>
2022-08-07 22:31:17 -0500

    refined geolocation code

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => { console.log("Listening in port 3000") });
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
+app.listen(3000, () => console.log("listening on port 3000"));
+
 
 app.post('/api', (request, response) => {
-    console.log(request.body);
+    console.log("Request Received");
+    const data = request.body;
+    console.log(data);
+
+
+    response.json({
+        status: 'success',
+        receivedLatitude: data.lat,
+        receivedLongitude: data.lon
+    })
 })
