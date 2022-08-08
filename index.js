58abae5dffa015ca4c803afed4cd58bdec385cc8
Charley griffin <boingr66@gmail.com>
2022-08-08 18:31:36 -0500

    added database functionality

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const app = express();
+const Datastore = require('nedb');
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 app.listen(3000, () => console.log("listening on port 3000"));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 
 app.post('/api', (request, response) => {
-    console.log("Request Received");
+    console.log('request received');
     const data = request.body;
-    console.log(data);
-
+    const lat = data.lat;
+    const lon = data.lon;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
 
     response.json({
         status: 'success',
-        receivedLatitude: data.lat,
-        receivedLongitude: data.lon
+        receivedLatitude: lat,
+        receivedLongitude: lon,
+        timestamp: timestamp
+
     })
 })
