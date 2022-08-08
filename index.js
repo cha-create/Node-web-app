008097259608380d2e965ae1176fc003a35dbe15
Charley griffin <boingr66@gmail.com>
2022-08-07 21:58:15 -0500

    added geolocation feature to server

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-app.listen(3000, () => { console.log('listening on port 3000') });
-app.use(express.static('public'));
+app.listen(3000, () => { console.log("Listening in port 3000") });
+app.use(express.static('public'))
+app.use(express.json({ limit: '1mb' }));
+
+app.post('/api', (request, response) => {
+    console.log(request.body);
+})
